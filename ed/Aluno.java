49ce45356ea4fd7f30b2e952cb7d61eb7bb7d535
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-10-08 14:16:07 -0300

    Finalização da segunda aula, lista simples

---
+++ ed/Aluno.java
@@ -0,0 +1,24 @@
+package ed;
+
+public class Aluno {
+    private String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object arg0) {
+        Aluno outro = (Aluno) arg0;
+        return outro.getNome().equals(this.nome);
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
